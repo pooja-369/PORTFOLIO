21cbdc79594df2e4b4486b26809f8ccf79221252
pooja-369 <pujagoyal369@gmail.com>
2022-06-18 13:12:03 +0530

    navabar responsivness

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-        if (scrollY > 20) {
-            $('.header').classList.add('sticky');
-        } else {
+    // $(window).scroll(function() {
+    //     if (scrollY > 20) {
+    //         $('.header').classList.add('sticky');
+    //     } else {
 
-        }
-    })
+    //     }
+    // })
+    $('.menu-btn').click(function() {
+        $('.header .menu').toggle("active");
+    });
 });
