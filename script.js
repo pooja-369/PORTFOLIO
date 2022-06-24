757eaf9aace1219434fcf27020a6b07318c998e0
pooja-369 <pujagoyal369@gmail.com>
2022-06-24 14:52:22 +0530

    updation color scheme

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-$(document).ready(function() {
-    // $(window).scroll(function() {
-    //     if (scrollY > 20) {
-    //         $('.header').classList.add('sticky');
-    //     } else {
-
-    //     }
-    // })
-    $('.menu-btn').click(function() {
-        $('.header .menu').toggle("active");
-    });
-});
+// document.querySelector()
